6e3c950bc9cab3dc1007f3ee9f29ffee2c4ad746
William Santos <williamdsw@outlook.com>
2021-08-28 18:11:37 -0300

    :ok_hand: Refactoring Loading Screen

---
+++ Assets/Scripts/Controllers/Menu/LoadingController.cs
@@ -0,0 +1,156 @@
+﻿using MVC.Enums;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Controllers.Menu
+{
+    public class LoadingController : MonoBehaviour
+    {
+        // || Inspector References
+
+        [Header("UI Elements")]
+        [SerializeField] private GameObject loadingPanel;
+        [SerializeField] private GameObject[] instructionsPanels;
+        [SerializeField] private Button[] gotoButtons;
+        [SerializeField] private Button continueButton;
+
+        [Header("Labels to Translate Global")]
+        [SerializeField] private TextMeshProUGUI keyboardLayoutHeader;
+        [SerializeField] private TextMeshProUGUI gamepadLayoutHeader;
+        [SerializeField] private TextMeshProUGUI powerUpHeader;
+        [SerializeField] private List<TextMeshProUGUI> movePaddleLabels;
+        [SerializeField] private List<TextMeshProUGUI> impulsePaddleLabels;
+        [SerializeField] private List<TextMeshProUGUI> pauseLabels;
+        [SerializeField] private List<TextMeshProUGUI> changeSongLabels;
+        [SerializeField] private List<TextMeshProUGUI> releaseBallShootLabels;
+
+        // || Config
+        private const float TIME_TO_WAIT = 1f;
+
+        // || Cached
+
+        private TextMeshProUGUI continueButtonLabel;
+
+
+        // Destructables singletons
+        private CursorController cursorController;
+        private FullScreenBackground fullScreenBackground;
+
+        private void Awake() => continueButtonLabel = continueButton.GetComponentInChildren<TextMeshProUGUI>();
+
+        private void Start()
+        {
+            // Find Objects
+            cursorController = FindObjectOfType<CursorController>();
+            fullScreenBackground = FindObjectOfType<FullScreenBackground>();
+
+            // Destroy some singletons
+            if (cursorController)
+            {
+                cursorController.DestroyInstance();
+            }
+
+            if (fullScreenBackground)
+            {
+                fullScreenBackground.DestroyInstance();
+            }
+
+            if (!GameStatusController.Instance.CameFromLevel)
+            {
+                loadingPanel.SetActive(false);
+                StartCoroutine(CallNextScene());
+            }
+            else
+            {
+                Translate();
+                BindEventListeners();
+            }
+        }
+
+        /// <summary>
+        /// Translates the UI
+        /// </summary>
+        private void Translate()
+        {
+            try
+            {
+                keyboardLayoutHeader.text = LocalizationController.Instance.GetWord(LocalizationFields.instructions_keyboardlayout);
+                gamepadLayoutHeader.text = LocalizationController.Instance.GetWord(LocalizationFields.instructions_gamepadlayout);
+                powerUpHeader.text = LocalizationController.Instance.GetWord(LocalizationFields.instructions_powerups);
+                movePaddleLabels.ForEach(label => label.text = LocalizationController.Instance.GetWord(LocalizationFields.instructions_movepaddle));
+                impulsePaddleLabels.ForEach(label => label.text = LocalizationController.Instance.GetWord(LocalizationFields.instructions_impulsepaddle));
+                pauseLabels.ForEach(label => label.text = LocalizationController.Instance.GetWord(LocalizationFields.instructions_pause));
+                changeSongLabels.ForEach(label => label.text = LocalizationController.Instance.GetWord(LocalizationFields.instructions_changesong));
+                releaseBallShootLabels.ForEach(label => label.text = LocalizationController.Instance.GetWord(LocalizationFields.instructions_releaseball));
+                continueButtonLabel.text = LocalizationController.Instance.GetWord(LocalizationFields.general_continue);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Bind event listeners to elements
+        /// </summary>
+        private void BindEventListeners()
+        {
+            try
+            {
+                // LAYOUT KEYBOARD PANEL - RIGHT BUTTON
+                gotoButtons[0].onClick.AddListener(() =>
+                {
+                    instructionsPanels[0].SetActive(false);
+                    instructionsPanels[1].SetActive(true);
+                });
+
+                // GAMEPAD KEYBOARD PANEL - LEFT BUTTON
+                gotoButtons[1].onClick.AddListener(() =>
+                {
+                    instructionsPanels[1].SetActive(false);
+                    instructionsPanels[0].SetActive(true);
+                });
+
+                // GAMEPAD KEYBOARD PANEL - RIGHT BUTTON
+                gotoButtons[2].onClick.AddListener(() =>
+                {
+                    instructionsPanels[1].SetActive(false);
+                    instructionsPanels[2].SetActive(true);
+                });
+
+                // POWER UPS PANEL - LEFT BUTTON
+                gotoButtons[3].onClick.AddListener(() =>
+                {
+                    instructionsPanels[2].SetActive(false);
+                    instructionsPanels[1].SetActive(true);
+                });
+
+                continueButton.onClick.AddListener(() =>
+                {
+                    continueButton.interactable = false;
+                    StartCoroutine(CallNextScene());
+                });
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Calls the next scene
+        /// </summary>
+        private IEnumerator CallNextScene()
+        {
+            yield return new WaitForSecondsRealtime(TIME_TO_WAIT);
+            float fadeOutLength = FadeEffect.Instance.GetFadeOutLength();
+            FadeEffect.Instance.FadeToLevel();
+            yield return new WaitForSecondsRealtime(fadeOutLength);
+            AsyncOperation operation = SceneManagerController.CallSceneAsync(GameStatusController.Instance.NextSceneName);
+        }
+    }
+}
