cca7cfdc5ec7ce5c9cbfc83d1e7ad5d608cf231f
William Santos <williamdsw@outlook.com>
2022-04-20 17:28:54 -0300

    :ok_hand: Creating ProjectInfo and ProjectEditorManager

---
+++ Assets/Editor/ProjectEditorManager.cs
@@ -0,0 +1,30 @@
+using MVC.Global;
+using System;
+using UnityEditor;
+using Utilities;
+
+public class ProjectEditorManager
+{
+    /// <summary>
+    /// Get project info to update editor values
+    /// </summary>
+    [MenuItem("Project/Update Properties", false, 1)]
+    protected static void UpdateProperties()
+    {
+        try
+        {
+            SerializedObject playerSettingsManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath(Configuration.Properties.ProjectSettings)[0]);
+            PlayerSettings.companyName = ProjectInfo.CompanyName;
+            PlayerSettings.productName = ProjectInfo.ProductName;
+            PlayerSettings.bundleVersion = ProjectInfo.BundleVersion;
+            PlayerSettings.macOS.buildNumber = ProjectInfo.BuildNumber;
+            PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Standalone, ProjectInfo.Identifier);
+            playerSettingsManager.ApplyModifiedProperties();
+            playerSettingsManager.Update();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/ProjectInfo.cs
@@ -0,0 +1,34 @@
+
+namespace Utilities
+{
+    /// <summary>
+    /// Information about this project
+    /// </summary>
+    public class ProjectInfo
+    {
+        /// <summary>
+        /// Project identifier
+        /// </summary>
+        public static string Identifier => "com.retrogemn.vaporbreaker";
+
+        /// <summary>
+        /// Company Name
+        /// </summary>
+        public static string CompanyName => "Retrogemn";
+
+        /// <summary>
+        /// Product Name
+        /// </summary>
+        public static string ProductName => "VAPORBREAKER";
+
+        /// <summary>
+        /// Project's version
+        /// </summary>
+        public static string BundleVersion => "3.0.0";
+
+        /// <summary>
+        /// Mac's build number
+        /// </summary>
+        public static string BuildNumber => "1";
+    }
+}
